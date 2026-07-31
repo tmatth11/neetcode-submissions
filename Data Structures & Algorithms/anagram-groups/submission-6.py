272ac121676467da05406565154283985952c498
tmatth11 <141206635+tmatth11@users.noreply.github.com>
2026-07-31 09:56:45 -0500

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Key: Character frequency, Value: Words
+        res = defaultdict(list)
+
+        for s in strs:
+            # Count frequency of each character
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            
+            res[tuple(count)].append(s)
+        
+        return list(res.values())
